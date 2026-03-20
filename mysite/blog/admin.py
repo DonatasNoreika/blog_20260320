712ecaec3fd68f09a140118934df6966de3113d5
DonatasNoreika <donoras@gmail.com>
2026-03-20 08:55:20 +0200

    +Aprašyti Post, Comment modelius, registruoti jį admin.py

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Comment
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_created']
+    list_filter = ['author', 'date_created']
+    search_fields = ['title', 'content']
+    inlines = [CommentInLine]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} ({self.date_created})"
