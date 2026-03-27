89f9bdfe4b75a5657cc894fbeba064ac12cd04fc
DonatasNoreika <donoras@gmail.com>
2026-03-27 08:38:01 +0200

    +Puslapiuoti įrašus

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
+from django.core.paginator import Paginator
 
 def posts(request):
+    posts = Post.objects.order_by('-pk')
+    paginator = Paginator(posts, per_page=2)
+    page_number = request.GET.get('page')
+    paged_posts = paginator.get_page(page_number)
     context = {
-        'posts': Post.objects.all().order_by('-pk'),
+        'posts': paged_posts,
     }
     return render(request, template_name="posts.html", context=context)
 
